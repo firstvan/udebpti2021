db39442083fe1e40701c3357d0ff4be05000e7bd
Orbán István <orban.istvan@tigra.hu>
2021-04-20 21:15:18 +0200

    init

---
+++ src/main/java/hu/depti/Calculator.java
@@ -0,0 +1,22 @@
+package hu.depti;
+
+public class Calculator {
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+  public int substract(int a, int b) {
+    return a - b;
+  }
+
+  public int multiply(int a, int b) {
+    return a * b;
+  }
+
+  public int divide(int a, int b) throws IllegalArgumentException {
+    if (b == 0) {
+      throw new IllegalArgumentException("Nullával való osztás nem értelmezett");
+    }
+    return a / b;
+  }
+}
